1cac86ed25a156da3396f7fc501b58310a67fed7
Harold Agnote <haroldagnote38@hotmail.com>
2016-12-11 20:38:56 -0800

    Started on Homework 8

---
+++ HomeworkAssignments/Homework8/Homework8.py
@@ -0,0 +1,22 @@
+# Homework 8
+# Harold Agnote
+# Harvey Han
+# Ivan Kim
+# Professor Tyler Nguyen
+# CECS 229 - Sec. 01
+# 12/15/2016
+
+from mat import Mat
+from matutil import listlist2mat
+
+
+def is_echelon(A):
+    for x in A.D[0]:
+
+
+L = listlist2mat([[2,4,0], [8,16,4], [0,0,7]])
+
+print(L)
+
+def echelon_solve(row_list, label_list, b):
+    return 0
